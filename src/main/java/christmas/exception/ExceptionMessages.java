3ff6fb3d72077bdfd78dc2b19621e73125fb30d5
koomchang <leahpar0401@gmail.com>
2023-11-10 04:12:28 +0900

    feat: 주문에 관한 검증 메소드 구현

--- src/main/java/christmas/exception/ExceptionMessages.java
+++ src/main/java/christmas/exception/ExceptionMessages.java
@@ -1,7 +1,8 @@
 package christmas.exception;
 
 public enum ExceptionMessages {
-    DATE_OUT_OF_RANGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    DATE_OUT_OF_RANGE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    ORDER_INVALID("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
     private static final String MESSAGE_FORMAT = "[ERROR] %s";
     private final String message;
--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,6 +1,10 @@
 package christmas.model;
 
+import static christmas.exception.ExceptionMessages.ORDER_INVALID;
+
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Order {
     private static final int MINIMUM_PRICE_FOR_EVENT = 10000;
@@ -8,7 +12,38 @@ public class Order {
     private final Map<Menu, Integer> menus;
 
     public Order(Map<Menu, Integer> menus) {
+        validate(menus);
         this.menus = menus;
+    }
+
+    public void validate(Map<Menu, Integer> menus) {
+        validateDuplicateMenu(menus);
+        validateMenuCount(menus);
+        validateOnlyBeverage(menus);
+    }
+
+    private void validateDuplicateMenu(Map<Menu, Integer> menus) {
+        Set<Menu> uniqueMenus = new HashSet<>(menus.keySet());
+        if (uniqueMenus.size() != menus.size()) {
+            throw new IllegalArgumentException(ORDER_INVALID.getMessage());
+        }
+    }
+
+    private void validateMenuCount(Map<Menu, Integer> menus) {
+        if (menus.size() > 20) {
+            throw new IllegalArgumentException(ORDER_INVALID.getMessage());
+        }
+    }
+
+    private void validateOnlyBeverage(Map<Menu, Integer> menus) {
+        if (hasOnlyBeverage(menus)) {
+            throw new IllegalArgumentException(ORDER_INVALID.getMessage());
+        }
+    }
+
+    private boolean hasOnlyBeverage(Map<Menu, Integer> menus) {
+        return menus.keySet().stream()
+                .allMatch(menu -> menu.getType().equals("음료"));
     }
 
     public boolean canParticipateInEvent() {
