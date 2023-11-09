1d06fce6674fb2b8a115a540618205a6463deaa0
koomchang <leahpar0401@gmail.com>
2023-11-10 04:32:25 +0900

    feat: 배지 출력 관련 메소드 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,11 +1,15 @@
 package christmas.view;
 
+import christmas.model.Badge;
+
 public class OutputView {
-    private static final String WELECOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String EVENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String EVENT_BADGE_MESSAGE = "<12월 이벤트 배지>" + LINE_SEPARATOR + "%s";
 
     public void printWelcomeMessage() {
-        System.out.println(WELECOME_MESSAGE);
+        System.out.println(WELCOME_MESSAGE);
     }
 
     public void printEventMessage(int visitDay) {
@@ -32,7 +36,7 @@ public class OutputView {
 
     }
 
-    public void printEventBadge() {
-
+    public void printEventBadge(Badge badge) {
+        System.out.printf(EVENT_BADGE_MESSAGE, badge);
     }
 }
