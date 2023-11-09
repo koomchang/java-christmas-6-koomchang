a6f77334c870eb037dec89845ced7af6cdb920d5
koomchang <leahpar0401@gmail.com>
2023-11-10 02:32:44 +0900

    feat: 날짜에 따라 요일을 계산하는 메소드 생성

--- src/main/java/christmas/model/Day.java
+++ src/main/java/christmas/model/Day.java
@@ -2,14 +2,15 @@ package christmas.model;
 
 public enum Day {
 
+    THURSDAY(false),
+    FRIDAY(false),
+    SATURDAY(true),
     SUNDAY(true),
     MONDAY(false),
     TUESDAY(false),
-    WEDNESDAY(false),
-    THURSDAY(false),
-    FRIDAY(false),
-    SATURDAY(true);
+    WEDNESDAY(false);
 
+    private static final int DAYS_IN_WEEK = 7;
     private final boolean isWeekend;
 
     Day(boolean isWeekend) {
@@ -18,5 +19,9 @@ public enum Day {
 
     public boolean isWeekend() {
         return isWeekend;
+    }
+
+    public static Day of(int date) {
+        return Day.values()[date % DAYS_IN_WEEK];
     }
 }
