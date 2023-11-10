a8b9c8cc6f64ab9ee1e97151cab2dc32d7bd3685
koomchang <leahpar0401@gmail.com>
2023-11-10 16:34:13 +0900

    fix: 출력에서 상수들이 출력 조건에 맞게 출력될 수 있도록 상수 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -9,10 +9,12 @@ public class OutputView {
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String WELCOME_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String EVENT_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!" + LINE_SEPARATOR;
-    private static final String ORDERED_MENU_MESSAGE = "<주문 메뉴>";
+    private static final String ORDERED_MENU_MESSAGE = LINE_SEPARATOR + "<주문 메뉴>";
     private static final String EACH_MENU_MESSAGE = "%s %d개" + LINE_SEPARATOR;
-    private static final String TOTAL_PRICE_BEFORE_DISCOUNT_MESSAGE = "<할인 전 총주문 금액>" + LINE_SEPARATOR + "%s원";
-    private static final String EVENT_BADGE_MESSAGE = "<12월 이벤트 배지>" + LINE_SEPARATOR + "%s";
+    private static final String TOTAL_PRICE_BEFORE_DISCOUNT_MESSAGE =
+            LINE_SEPARATOR + "<할인 전 총주문 금액>" + LINE_SEPARATOR + "%s원" + LINE_SEPARATOR;
+    private static final String GIFT_MENU_MESSAGE = LINE_SEPARATOR + "<증정 메뉴>" + LINE_SEPARATOR + "%s";
+    private static final String EVENT_BADGE_MESSAGE = LINE_SEPARATOR + "<12월 이벤트 배지>" + LINE_SEPARATOR + "%s";
 
     private final DecimalFormat decimalFormat = new DecimalFormat("#,###");
 
@@ -36,8 +38,12 @@ public class OutputView {
         System.out.printf(TOTAL_PRICE_BEFORE_DISCOUNT_MESSAGE, formatPrice);
     }
 
-    public void printGiftMenu() {
-
+    public void printGiftMenu(boolean isGiftEventEligible) {
+        if (isGiftEventEligible) {
+            System.out.printf(GIFT_MENU_MESSAGE, Menu.gift() + " 1개");
+            return;
+        }
+        System.out.printf(GIFT_MENU_MESSAGE, "없음");
     }
 
     public void printBenefits() {
