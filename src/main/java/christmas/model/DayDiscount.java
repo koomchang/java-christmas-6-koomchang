90428f88718d90c29c01fce14044a8a8ffe7a9c4
koomchang <leahpar0401@gmail.com>
2023-11-12 20:41:28 +0900

    feat: 평일, 주말 할인 이벤트 클래스 구현

--- src/main/java/christmas/model/DayDiscount.java
+++ src/main/java/christmas/model/DayDiscount.java
@@ -1,2 +1,15 @@
-package christmas.model;public class DayDiscount {
+package christmas.model;
+
+public class DayDiscount {
+    private static final int DISCOUNT_RATE = 2023;
+
+    private final Money discountAmount;
+
+    public DayDiscount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public Money calculate(Date eventDate, int menuCount) {
+        return new Money(menuCount * DISCOUNT_RATE);
+    }
 }
