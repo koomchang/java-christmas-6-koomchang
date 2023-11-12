ce964787658806a007a6eb1d4017eb7835de9347
koomchang <leahpar0401@gmail.com>
2023-11-12 23:02:05 +0900

    refactor: 할인정책 인터페이스 상속을 이용하여 크리스마스 디데이 이벤트 클래스 리팩토링

--- src/main/java/christmas/model/ChristmasDiscountPolicy.java
+++ src/main/java/christmas/model/ChristmasDiscountPolicy.java
@@ -1,17 +1,12 @@
 package christmas.model;
 
-public class ChristmasDiscountPolicy {
+public class ChristmasDiscountPolicy implements DiscountPolicy {
     private static final int CHRISTMAS_BASIC_DISCOUNT = 1000;
     private static final int CHRISTMAS_DISCOUNT_RATE = 100;
     private static final int NO_DISCOUNT = 0;
 
-    private final Money discountAmount;
-
-    public ChristmasDiscountPolicy(Money discountAmount) {
-        this.discountAmount = discountAmount;
-    }
-
-    public Money calculate(Date eventDate) {
+    @Override
+    public Money calculate(Date eventDate, Order order) {
         int discountValue = CHRISTMAS_BASIC_DISCOUNT + ((eventDate.date() - 1) * CHRISTMAS_DISCOUNT_RATE);
         if (!Date.isChristmasEventPeriod(eventDate)) {
             return new Money(NO_DISCOUNT);
