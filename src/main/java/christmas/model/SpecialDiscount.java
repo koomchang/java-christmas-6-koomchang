7ae8040f093735aec72d704ff32f567dcda9df4f
koomchang <leahpar0401@gmail.com>
2023-11-12 20:44:36 +0900

    feat: 특별 할인 이벤트 클래스 구현

--- src/main/java/christmas/model/SpecialDiscount.java
+++ src/main/java/christmas/model/SpecialDiscount.java
@@ -1,2 +1,21 @@
-package christmas.model;public class SpecialDiscount {
+package christmas.model;
+
+public class SpecialDiscount {
+    private static final int SPECIAL_DISCOUNT = 1000;
+    private static final int NO_DISCOUNT = 0;
+
+    private final Money discountAmount;
+
+    public SpecialDiscount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public Money calculate(Date eventDate) {
+        for (StarredDate specialDate : StarredDate.values()) {
+            if (Date.isStarred(eventDate)) {
+                return new Money(SPECIAL_DISCOUNT);
+            }
+        }
+        return new Money(NO_DISCOUNT);
+    }
 }
