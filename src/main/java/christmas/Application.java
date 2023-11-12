831b49cd23cb8c6f891e0af217ff1cf57800c84c
koomchang <leahpar0401@gmail.com>
2023-11-12 23:04:51 +0900

    feat: EventPlanner 클래스의 런타임시 구현체를 Application에서 주입

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,6 +1,10 @@
 package christmas;
 
 import christmas.controller.ChristmasPromotionController;
+import christmas.model.ChristmasDiscountPolicy;
+import christmas.model.DayDiscountPolicy;
+import christmas.model.EventPlanner;
+import christmas.model.GiftEventPolicy;
 import christmas.validator.OrderMenuAndCountInputValidator;
 import christmas.validator.VisitDateInputValidator;
 import christmas.view.InputView;
@@ -8,12 +12,20 @@ import christmas.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        EventPlanner eventPlanner = new EventPlanner(
+                new ChristmasDiscountPolicy(),
+                new DayDiscountPolicy(),
+                new GiftEventPolicy()
+        );
+
         ChristmasPromotionController christmasPromotionController = new ChristmasPromotionController(
                 new OutputView(),
                 new InputView(
                         new VisitDateInputValidator(),
                         new OrderMenuAndCountInputValidator()
-                ));
+                ),
+                eventPlanner);
+
         christmasPromotionController.run();
     }
 }
