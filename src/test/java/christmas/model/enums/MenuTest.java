76986118e1af498cb0d3be7df37f250152103a83
koomchang <leahpar0401@gmail.com>
2023-11-13 23:47:46 +0900

    test: 증정 상품이 샴페인인지 확인하는 테스트

--- src/test/java/christmas/model/enums/MenuTest.java
+++ src/test/java/christmas/model/enums/MenuTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -25,4 +26,9 @@ class MenuTest {
         ).withMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 
+    @DisplayName("증정 상품은 샴페인이다")
+    @Test
+    void getGift() {
+        assertThat(Menu.gift()).isEqualTo(Menu.샴페인);
+    }
 }
