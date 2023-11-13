e892767b8dbfdb9840cb4c7d35845c572fa1d520
koomchang <leahpar0401@gmail.com>
2023-11-13 23:32:56 +0900

    test: 잔액끼리 더하고 뺄 수 있는지 테스트

--- src/test/java/christmas/model/vo/MoneyTest.java
+++ src/test/java/christmas/model/vo/MoneyTest.java
@@ -1,5 +1,6 @@
 package christmas.model.vo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +17,26 @@ class MoneyTest {
         assertThatIllegalArgumentException().isThrownBy(
                 () -> money = new Money(value)
         ).withMessage("[ERROR] 잔액은 음수가 될 수 없습니다.");
+    }
+
+    @DisplayName("잔액끼리 더할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {2000})
+    void plus(int value) {
+        money = new Money(1000);
+        Money moneyToPlus = new Money(value);
+        Money expected = new Money(3000);
+        assertThat(money.plus(moneyToPlus)).isEqualTo(expected);
+    }
+
+    @DisplayName("잔액끼리 뺄 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1000})
+    void minus(int value) {
+        money = new Money(2000);
+        Money moneyToMinus = new Money(value);
+        Money expected = new Money(1000);
+        assertThat(money.minus(moneyToMinus)).isEqualTo(expected);
     }
 
 }
