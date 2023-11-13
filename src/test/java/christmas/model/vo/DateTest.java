7953682317d2b10bfcd2b67ac4deb49060589e20
koomchang <leahpar0401@gmail.com>
2023-11-13 23:14:49 +0900

    test: 날짜가 특별 할인 이벤트에 해당하는 날인지 확인하는 테스트

--- src/test/java/christmas/model/vo/DateTest.java
+++ src/test/java/christmas/model/vo/DateTest.java
@@ -18,5 +18,20 @@ class DateTest {
                 () -> date = new Date(input)
         ).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
-    
+
+    @DisplayName("특별 할인이벤트에 해당하는 날인지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {3, 10, 17, 24, 25, 31})
+    void isSpecial(int input) {
+        date = new Date(input);
+        assertThat(date.isSpecial()).isTrue();
+    }
+
+    @DisplayName("특별 할인이벤트에 해당하지 않는 날인지 확인한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1, 5, 15, 20, 30})
+    void isNotSpecial(int input) {
+        date = new Date(input);
+        assertThat(date.isSpecial()).isFalse();
+    }
 }
