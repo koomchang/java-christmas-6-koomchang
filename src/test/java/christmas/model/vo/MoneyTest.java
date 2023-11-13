cc10fd124e94dfa6b0b61787610e46cda30e00cb
koomchang <leahpar0401@gmail.com>
2023-11-13 23:30:54 +0900

    test: Money가 음수가 될 수 없는지 검증 테스트

--- src/test/java/christmas/model/vo/MoneyTest.java
+++ src/test/java/christmas/model/vo/MoneyTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package christmas.model.vo;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class MoneyTest {
-  
+    private Money money;
+
+    @DisplayName("잔액은 음수가 될 수 없다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -1000})
+    void validateIfNegative(int value) {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> money = new Money(value)
+        ).withMessage("[ERROR] 잔액은 음수가 될 수 없습니다.");
+    }
+
 }
